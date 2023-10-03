25328d9881b14386e2fc16ff22fca345cca5c526
aashnamehta810 <aashna.m@manektech.com>
2023-10-03 19:07:38 +0530

    latest commit

--- src/Sections/SharedSections/TraitCards.vue
+++ src/Sections/SharedSections/TraitCards.vue
@@ -1,23 +1,21 @@
 <template>
   <div class="w-full flex flex-col">
-    <img
-        class="w-full"
-        :src="item.img"
-        alt="">
+    <img class="w-full" :src="item.img" alt="" />
     <p
-        :class="[alignLeft ? 'ml-4 lg:ml-0': 'ml-4 lg:ml-12']"
-        class="mt-9 font-naga text-[24px] lg:text-[30px] -tracking-[.6px] text-night ">
+      :class="[alignLeft ? 'ml-4 lg:ml-0' : 'ml-4 lg:ml-12']"
+      class="mt-9 font-naga text-[24px] lg:text-[30px] -tracking-[.6px] text-night"
+    >
       {{ item.title }}
     </p>
     <p
-        :class="[alignLeft ? 'ml-4 lg:ml-0': 'ml-4 lg:ml-12']"
-        class="mt-4 lg:mt-[1px]">
+      :class="[alignLeft ? 'ml-4 lg:ml-0' : 'ml-4 lg:ml-12']"
+      class="mt-4 lg:mt-[1px]"
+    >
       {{ item.desc }}
     </p>
   </div>
 </template>
 
 <script setup>
-
-const props = defineProps(['item', 'alignLeft'])
+const props = defineProps(["item", "alignLeft"]);
 </script>

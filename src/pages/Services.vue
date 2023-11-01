6b425343df4746b80ca8afaafb5296c190e7b8d5
aashnamehta810 <aashna.m@manektech.com>
2023-11-01 17:53:35 +0530

    dev 01-11

--- src/pages/Services.vue
+++ src/pages/Services.vue
@@ -1,16 +1,14 @@
 <template>
-  <services-banner/>
+  <services-banner />
   <service-catalogue />
   <find-out-more />
-  <spectrum-section />
+  <!-- <spectrum-section /> -->
   <test-us />
   <questions-banner />
   <qualities title="Discover the XCL Labs Difference" />
 </template>
 
-
 <script setup>
-
 import ServicesBanner from "@/Sections/ServicesSection/ServicesBanner.vue";
 import ServiceCatalogue from "@/Sections/ServicesSection/ServiceCatalogue.vue";
 import FindOutMore from "@/Sections/ServicesSection/FindOutMore.vue";
